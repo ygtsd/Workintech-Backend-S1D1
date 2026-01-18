4d9373b803facb140c50dd4c79b6c28bf6bf2e77
Yiğit Sadi <yigitsadi24@gmail.com>
2026-01-19 00:17:19 +0300

    Tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,40 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+
+        if (!isBarking) {
+            return false;
+        }
+
+        return (clock < 8 || clock > 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        return temp >= 25 && temp <= (isSummer ? 45 : 35);
     }
 
+
     public static double area(double width, double height) {
-       
+       if (width < 0 || height < 0 ) {
+           return -1;
+       }
+
+       return width*height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius*radius*Math.PI;
     }
 }
